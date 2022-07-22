58b5045492d4a56d5d52c1bf5cc6167aeb4aeeed
chenjl <chenjl@augurit.com>
2022-07-22 11:38:30 +0800

    添加rsa和md5爆破

--- MD5_demo.py
+++ MD5_demo.py
@@ -8,3 +8,20 @@ mm = hashlib.md5((cook + m_filename.hexdigest()).encode())
 
 print(m_filename.hexdigest())
 print(mm.hexdigest())
+
+
+'''
+MD5 爆破
+'''
+
+demo='flag{www_shiyanbar_com_is_very_good_'
+check = '38e4c352809e150186920aac37190cbc'
+ 
+for i in range(32,126):
+    for j in range(32,126):
+        for k in range(32,126):
+            for m in range(32,126):
+                tmp = demo + chr(i) + chr(j) + chr(k) + chr(m) + '}'
+                hash = hashlib.md5(tmp.encode('utf8')).hexdigest()
+                if check == hash:
+                    print(tmp)
